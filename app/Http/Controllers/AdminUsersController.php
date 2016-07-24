a711052f0fa9a289442122f7e827c1fe9932bc9e
karim <laravel.laravel1@gmail.com>
2016-07-25 01:43:07 +0200

    users with photos working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -42,9 +43,31 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
+        //return $request->all();
+//        User::create($request->all());
+//        return redirect('/admin/users');
 
-        User::create($request->all());
-        return redirect('/admin/users');
+
+//        if($request->file('photo_id')){
+//
+//            return "photo exist!";
+//        }
+
+
+            $input=$request->all();
+
+        if($file=$request->file('photo_id')){
+
+            $name=time().$file->getClientOriginalName();
+            $file->move('images',$name);
+            $photo=Photo::create(['file'=>$name]);//
+            $input['photo_id']=$photo->id;
+
+        }
+            $input['password']=bcrypt($request->password);
+            User::create($input);
+
+
     }
 
     /**
